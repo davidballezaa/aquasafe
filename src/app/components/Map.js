f65f437be7e30404aa2b3d0c1bdeb564e44398d3
David Balleza Ayala <davidballezaa@gmail.com>
2023-11-10 19:18:02 -0600

    Add dynamic pins to the map

--- src/app/components/Map.js
+++ src/app/components/Map.js
@@ -2,30 +2,43 @@
 
 import Globe from 'react-globe.gl';
 import { useRouter } from 'next/navigation';
+import { useState, useEffect } from 'react';
 
 export default function Map() {
     const router = useRouter();
-    const myData = [
-        {
-            lat: 25,
-            lng: -100,
-            id: 1,
-            label: 'Hola',
-            color: 'red',
-        },
-    ];
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('/api/map')
+            .then((res) => res.json())
+            .then((pins) => {
+                const newData = pins?.data.map((pin) => {
+                    return {
+                        id: pin.id,
+                        lat: pin.latitud,
+                        lng: pin.longitud,
+                        label: '',
+                        color: pin.status === 'contaminado' ? 'red' : 'green',
+                    };
+                });
+                setData(newData);
+            });
+    }, []);
+
     return (
-        <Globe
-            backgroundColor="white"
-            globeImageUrl="//unpkg.com/three-globe@2.30.0/example/img/earth-blue-marble.jpg"
-            width={500}
-            height={500}
-            labelsData={myData}
-            labelText="label"
-            labelColor={(pin) => pin.color}
-            labelSize={0.5}
-            labelDotRadius={1}
-            onLabelClick={(pin) => router.push(`/detail/${pin.id}`)}
-        />
+        <>
+            <Globe
+                backgroundColor="white"
+                globeImageUrl="//unpkg.com/three-globe@2.30.0/example/img/earth-blue-marble.jpg"
+                width={500}
+                height={500}
+                labelsData={data}
+                labelText="label"
+                labelColor={(pin) => pin.color}
+                labelSize={0.5}
+                labelDotRadius={1}
+                onLabelClick={(pin) => router.push(`/detail/${pin.id}`)}
+            />
+        </>
     );
 }
