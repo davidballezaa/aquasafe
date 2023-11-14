0d339b7efcbefb46f0c5e383bffb9863371c0a2e
David Balleza Ayala <davidballezaa@gmail.com>
2023-11-14 13:31:24 -0600

    Add graphs to detail page with id data

--- src/app/detail/[id]/page.js
+++ src/app/detail/[id]/page.js
@@ -1,7 +1,50 @@
+'use client';
+import { useEffect, useState } from 'react';
+
+import Graph from './components/graph';
+
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+} from 'recharts';
+
 export default function Home({ params }) {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch(`/api/detail/${params.id}`)
+            .then((res) => res.json())
+            .then((pins) => {
+                const newData = pins?.data.map((pin) => {
+                    return {
+                        // id: pin.id_Aquasafe,
+                        date: pin.fecha,
+                        ir: pin.lectura_ir,
+                        ph: pin.lectura_ph,
+                        temp: pin.lectura_temp,
+                        // lat: pin.latitud,
+                        // lng: pin.longitud,
+                        // color: pin.estado == 'potable' ? 'green' : 'red',
+                    };
+                });
+                setData(newData);
+            });
+    }, []);
+
     return (
         <main>
-            <p>{params.id}</p>
+            {data && (
+                <div className="graphs">
+                    <Graph data={data} attribute={'ir'} color={'#FF0000'} />
+                    <Graph data={data} attribute={'ph'} color={'#00FF00'} />
+                    <Graph data={data} attribute={'temp'} color={'#0000FF'} />
+                </div>
+            )}
         </main>
     );
 }
