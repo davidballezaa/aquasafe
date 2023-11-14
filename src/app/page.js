1710f1fea1eccf2492f2a9e7cc4502111eb05945
David Balleza Ayala <davidballezaa@gmail.com>
2023-11-14 14:55:48 -0600

    Add filter functionality to Map component

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,26 @@
+'use client';
 import Map from './components/Map';
+import { useState } from 'react';
 
 export default function Home() {
+    const [filter, setFilter] = useState('');
+
+    const handleFilterChange = (e) => {
+        setFilter(e.target.value);
+    };
+
     return (
         <main>
             <div className="map flex flex-col items-center box-decoration-slice bg-gradient-to-r from-slate-700 to-slate-900">
                 <div className="flex flex-row space-x-1">
                     <p className="text-white">Filtrar: </p>
-                    <select>
-                        <option>Puntos Rojos</option>
-                        <option>Puntos Verdes</option>
+                    <select onChange={handleFilterChange} value={filter}>
+                        <option value="">Sin filtro</option>
+                        <option value="red">No potables</option>
+                        <option value="green">Potables</option>
                     </select>
                 </div>
-                <Map />
+                <Map filter={filter} />
             </div>
         </main>
     );
