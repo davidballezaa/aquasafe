ce4d0e3b6d92be800913fd23a11e0ba513fd8e7b
David Balleza Ayala <davidballezaa@gmail.com>
2023-11-28 18:33:02 -0600

    Add 'direccion' field to SELECT query in route.js

--- src/app/api/detail/[id]/route.js
+++ src/app/api/detail/[id]/route.js
@@ -4,7 +4,7 @@ export async function GET(request, { params }) {
     const aquaSafeId = params.id;
 
     const res = await executeQuery({
-        query: 'SELECT c.id_Aquasafe, c.latitud, c.longitud, p.lectura_ph, t.lectura_temp, i.lectura_ir, m.fecha FROM coords as c, muestra_a as m, ph as p, ir as i, temp as t WHERE c.id_Aquasafe = m.id_Aquasafe AND m.id_PH = p.id_PH AND m.id_Temp = t.id_Temp AND m.id_IR = i.id_IR AND m.id_Aquasafe = ?',
+        query: 'SELECT c.id_Aquasafe, c.latitud, c.longitud, p.lectura_ph, t.lectura_temp, i.lectura_ir, m.fecha, c.direccion FROM coords as c, muestra_a as m, ph as p, ir as i, temp as t WHERE c.id_Aquasafe = m.id_Aquasafe AND m.id_PH = p.id_PH AND m.id_Temp = t.id_Temp AND m.id_IR = i.id_IR AND m.id_Aquasafe = ?',
         values: [aquaSafeId],
     });
 
